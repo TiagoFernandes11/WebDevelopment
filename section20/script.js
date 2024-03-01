b2655d619df0e38fd70ce15f5daaef81f3a775bd
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2024-03-01 12:07:49 -0300

    Animation and audio playing

--- section20/script.js
+++ section20/script.js
@@ -1,7 +1,16 @@
-buttonColors = ["red", "blue", "green", "yellow"];
+buttonColors = ["green", "red", "yellow", "blue"];
 gamePattern = [];
 
 function nextSequence() {
   randomChosenColor = buttonColors[Math.floor(Math.random() * 4)];
   gamePattern.push(randomChosenColor);
 }
+
+nextSequence();
+
+gamePattern.forEach((color) => {
+  $(`#${color}`).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+  var audio = new Audio(`sounds/${color}.mp3`);
+  audio.play();  
+});
+
