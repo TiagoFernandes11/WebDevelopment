292d39fb0c8caefb46c5da04fa392cfebc783f36
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2024-03-02 21:48:10 -0300

    section 23.3

--- section23/2.2 Native Modules/index.js
+++ section23/2.2 Native Modules/index.js
@@ -5,7 +5,14 @@ fs.writeFile("message.txt", "Hello Node", (err) => {
   console.log("The file has been saved!");
 });
 
+fs.appendFile('message.txt', '\nData appended', function (err) {
+    if (err) throw err;
+    console.log('Saved!');
+  });
+
 fs.readFile("message.txt", "utf8", (err, data) => {
   if (err) throw err;
   console.log(data);
 });
+
+
---
+++ section23/2.3 NPM/index.js
@@ -0,0 +1,7 @@
+import generateName from 'sillyname';
+import superheroes from 'superheroes';
+
+console.log(`My programmer name is ${generateName()}`);
+console.log(`And my super-hero name is ${superheroes.random()}`)
+
+
