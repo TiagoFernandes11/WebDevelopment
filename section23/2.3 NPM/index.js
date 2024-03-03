cf21272e2dba9bd26aa848737dd3d1030bfc62cb
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2024-03-03 09:36:18 -0300

    section 23.4 start

--- section23/2.3 NPM/index.js
+++ section23/2.3 NPM/index.js
@@ -1,7 +1,7 @@
-import generateName from 'sillyname';
-import superheroes from 'superheroes';
+import generateName from "sillyname";
+import superheroes from "superheroes";
+import dogNames from "dog-names";
 
 console.log(`My programmer name is ${generateName()}`);
-console.log(`And my super-hero name is ${superheroes.random()}`)
-
-
+console.log(`And my super-hero name is ${superheroes.random()}`);
+console.log(`My dog's name is ${dogNames.maleRandom()}`);
