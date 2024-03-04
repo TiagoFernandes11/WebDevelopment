e78b8ed13d1fc516dc6f1cd94b8bfe43246d035a
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2024-03-04 19:50:41 -0300

    section 24.4 completed

--- section24/3.4 Middleware/index3.js
+++ section24/3.4 Middleware/index3.js
@@ -3,7 +3,11 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.use(logger);
+app.use((req, res, next) => {
+  console.log("Request method: " + req.method);
+  console.log("Request URL: " + req.url);
+  next();
+});
 
 app.get("/", (req, res) => {
   res.send("Hello");
