fcdca910d4a21059eb09d5bd01a8d6ff151353db
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2024-03-05 20:01:03 -0300

    section 25.0 exercise EJS

--- section25/4.0 EJS/index.js
+++ section25/4.0 EJS/index.js
@@ -1,12 +1,24 @@
 import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import ejs from "ejs";
 import morgan, { format } from "morgan";
+
 const app = express();
 const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const currentDay = new Date().getDay(); //sexto e setimo dia final de semana
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(morgan("tiny"));
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello world</h1>");
+  res.render(__dirname + "/views/index.ejs", { currentDay: currentDay });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
