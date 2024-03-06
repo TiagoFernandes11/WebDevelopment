f692bcd7eb13ae8f75b9eb3c950185c97f4fea5f
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2024-03-06 17:11:21 -0300

    capstone project init

---
+++ section26/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const port = 3000;
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.get("/", (req, res) => {
+  res.render(__dirname + "/views/index.ejs");
+});
+
+app.listen(port, () => {
+  console.log("Server running on port " + port);
+});
